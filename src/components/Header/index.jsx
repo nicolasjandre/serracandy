74fc4377145efb904997ff857559b5db7846a48f
Marlon Viana Souza <marlon_vs@yahoo.com>
2023-06-12 19:53:19 -0300

    insere modal do carrinho, insere db e insere cardshop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import { CartContext } from '../../contexts/CartContext';
+import { useContext } from 'react';
 
 import './style.css';
+import { IconButton } from '@mui/material';
+
 export function Header() {
+    const {setCartModalOpen} = useContext(CartContext)
+
     return (
         <div className='container'>
             <ul className='leftnav'>
-                <Link className='link' to="/">HOME</Link>
+                <Link className='link' to="/">INÍCIO</Link>
                 <Link className='link' to="/shop">DOCES +</Link>
             </ul>
             
@@ -19,7 +25,17 @@ export function Header() {
             <ul className='rightnav'>
                 <Link className='link' to="/sobre">SOBRE</Link>
                 <Link className='link' to="/contato">CONTATO</Link>
-                <Link className='link' to="/checkout"><ShoppingCartOutlinedIcon /></Link>
+                <IconButton
+                onClick={() => setCartModalOpen(true)}
+                    // @ts-ignore
+                    aria-label="Adicionar ao carrinho"
+                    sx={{
+                        "&:hover": { backgroundColor: "transparent" },
+                        cursor: "pointer",
+                        p: "0",
+                        color: "#000000"
+                    }}
+                ><ShoppingCartOutlinedIcon/></IconButton>
                 <Link className='link' to="/login"><LoginOutlinedIcon /></Link>
             </ul>
         </div>
